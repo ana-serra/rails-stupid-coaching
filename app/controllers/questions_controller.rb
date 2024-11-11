3930361221a77732196945024be75f59d053950a
Ana Serra <ana.nogueiraserra@icloud.com>
2024-11-11 15:21:42 +0100

    done

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @questions = params[:question]
+    @answer =  if @questions == "I am going to work"
+        "Great!"
+      elsif @questions.include?("?")
+        "Silly question, get dressed and go to work!"
+      else
+        "I don't care, get dressed and go to work!"
+      end
+  end
+
+end
